7a3f5af19ae26ad94f84d386fcd1a4421a2a2328
zsofia95 <33411959+zsofia95@users.noreply.github.com>
2017-11-27 09:47:53 +0200

    Menu

--- app/src/main/java/ro/sapientia/ms/nagyizabella/sapiadvertiserizabellazsofia/MainActivity.java
+++ app/src/main/java/ro/sapientia/ms/nagyizabella/sapiadvertiserizabellazsofia/MainActivity.java
@@ -1,13 +1,17 @@
 package ro.sapientia.ms.nagyizabella.sapiadvertiserizabellazsofia;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button mSignInButton;
     private Button mGuestButton;
@@ -72,5 +76,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
 }
 
--- app/src/main/java/ro/sapientia/ms/nagyizabella/sapiadvertiserizabellazsofia/MenuActivity.java
+++ app/src/main/java/ro/sapientia/ms/nagyizabella/sapiadvertiserizabellazsofia/MenuActivity.java
@@ -112,4 +112,5 @@ public class MenuActivity extends AppCompatActivity {
             }
         }
     }
+    
 }
