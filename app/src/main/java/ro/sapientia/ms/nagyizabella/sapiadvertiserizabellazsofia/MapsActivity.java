58f484ca5b9d500dc2aa3d9436f6b31f712557de
zsofia95 <33411959+zsofia95@users.noreply.github.com>
2018-01-03 20:08:07 +0200

    Delete

--- app/src/main/java/ro/sapientia/ms/nagyizabella/sapiadvertiserizabellazsofia/MapsActivity.java
+++
@@ -1,8 +0,0 @@
-package ro.sapientia.ms.nagyizabella.sapiadvertiserizabellazsofia;
-
-import android.support.v4.app.FragmentActivity;
-
-
-public class MapsActivity extends FragmentActivity //implements OnMapReadyCallback//
-{
-}
